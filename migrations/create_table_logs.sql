d78559bbabe9bcf67b198838f3a8af016a87a003
Philippe Lafoucrière <philippe.lafoucriere@tech-angels.com>
2016-11-04 17:04:11 -0400

    Travis pg 9.5 is buggy

--- migrations/create_table_logs.sql
+++ migrations/create_table_logs.sql
@@ -2,6 +2,6 @@ CREATE TABLE logs (
     id SERIAL,
     level smallint NOT NULL,
     message text NOT NULL,
-    message_data jsonb NOT NULL,
+    message_data json NOT NULL,
     created_at timestamp with time zone NOT NULL
 );
